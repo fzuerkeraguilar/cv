4252275e9c1808f620d82368bfe697bef9d032e2
Fabian Zürker Aguilar <fzuerker@gmail.com>
2023-06-13 17:01:39 +0200

    Add personal information

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,68 +1,63 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Fabian Zürker Aguilar',
+  title: 'Informatikstudent am KIT',
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
-                </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
+    "Ak. Grad": {
+      icon: "graduation-cap",
+      text: "Abitur",
+    },
+    "Geburtstag": {
+      icon: "asterisk",
+      text: "15.08.2002",
+    },
+    "Wohnort": {
+      icon: "home",
+      text: "Karlsruhe",
+    },
+    "E-Mail": {
+      icon: "envelope",
+      text: '<a href="mailto:fzuerker@gmail.com">fzuerker@gmail.com</a>',
+    },
   },
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 75],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 75],
-    ['Literature', 100],
-    ['Influence', 100],
+    ['Java', 100],
+    ['Python', 100],
+    ['C++', 60],
+    ['TypeScript', 40],
+    ['Docker', 70],
+    ['CI/CD', 20]
   ],
   positions: [
+  ],
+  education: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      title: "Informatik B.Sc.",
+      org: "Karlsruher Institut für Technologie",
+      datespan: "Oktober 2020 - März 2024",
+      contents: ``,
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      title: "Abitur",
+      org: "Heisenberg Gymnasium Karlsruhe",
+      datespan: "2012 - 2020",
+      contents: `Note: 1,3`,
     },
-    {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
-    }
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
+      header: "Sprachkenntnisse",
       contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+* Deutsch (Muttersprache)
+* Englisch (B2-C1) 
+* Spanisch (B1)
+`,
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
+      header: "Hobbies",
       contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
-  ]
+*	Fahrradfahren
+* Modellbau
+      `,
+    },
+  ],
 };
